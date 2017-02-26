f899885773bb5ba655a1eba9d9eeacba8fce946a
SAM <samanthamorales8267@gmail.com>
2017-02-25 18:04:58 -0800

    Generated three Pictures

--- Bus-Mall.js
+++ Bus-Mall.js
@@ -25,7 +25,7 @@
 
 
 //Objects stored in an array.
-var allProductsNames = ['bag', 'banana', 'bathroom','boots','breakfast','bubblegum','chair','cthulhu', 'dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wineglass'];
+var allProductsNames = ['bag', 'banana', 'bathroom','boots','breakfast','bubblegum','chair','cthulhu', 'dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
 
 var allProducts = [];
 
@@ -52,41 +52,43 @@ console.log(button);
 
 button.addEventListener('click', function(event){
   console.log('hit here?');
-  [random_imglink()];
-})
+  random_imglink();
+});
+
 function random_imglink(){
-  var myImages= [{src:'img/bag.jpg'},
-  {src:'img/banana.jpg'},
-  {src:'img/bathroom.jpg'},
-  {src:'img/boots.jpg'},
-  {src:'img/breakfast.jpg'},
-  {src:'img/bubblegum.jpg'},
-  {src:'img/chair.jpg'},
-  {src:'img/cthulhu.jpg'},
-  {src:'img/dog-duck.jpg'},
-  {src:'img/dragon.jpg'},
-  {src:'img/pet-sweep.jpg'},
-  {src:'img/pen.jpg'},
-  {src:'img/scissors.jpg'},
-  {src:'img/shark.jpg'},
-  {src:'img/sweep.jpg'},
-  {src:'img/tauntaun.jpg'},
-  {src:'img/unicorn.jpg'},
-  {src:'img/usb.jpg'},
-  {src:'img/water-can.jpg'},
-  {src:'img/wine-glass.jpg'}
-];
+//   var myImages= [
+//     {src:'img/bag.jpg'},
+//   {src:'img/banana.jpg'},
+//   {src:'img/bathroom.jpg'},
+//   {src:'img/boots.jpg'},
+//   {src:'img/breakfast.jpg'},
+//   {src:'img/bubblegum.jpg'},
+//   {src:'img/chair.jpg'},
+//   {src:'img/cthulhu.jpg'},
+//   {src:'img/dog-duck.jpg'},
+//   {src:'img/dragon.jpg'},
+//   {src:'img/pet-sweep.jpg'},
+//   {src:'img/pen.jpg'},
+//   {src:'img/scissors.jpg'},
+//   {src:'img/shark.jpg'},
+//   {src:'img/sweep.jpg'},
+//   {src:'img/tauntaun.jpg'},
+//   {src:'img/unicorn.jpg'},
+//   {src:'img/usb.jpg'},
+//   {src:'img/water-can.jpg'},
+//   {src:'img/wine-glass.jpg'}
+// ];
 
 // img1: documentElementById('img1',[0]),
 // img2: documentElementById('img2',[1]),
 // img3: documentElementById('img3',[2])
 
 
-
-var preStuff = []];
-for (var i = 0; i < myImages.length; i++) {
+//Calling all products in array and changing the filepath.
+var preStuff = [];
+for (var i = 0; i <  allProducts.length; i++) {
   preStuff[i] = new Image();
-  preStuff [i].src =myImages[i].src;
+  preStuff[i].src = allProducts[i].filepath;
 }
 
 //Create random image number
@@ -98,17 +100,21 @@ sam = Math.floor(Math.random() * (max - min + 1)) + min;
   return preStuff[sam];
 }
 
-// 0 being the first image and preStuff.length - 1 is  last image.
-
-var newImage = getRandomInt(0, preStuff.length - 1);
-
 //Removes the previous images.
 var images = document.getElementsByTagName('img');
-var l = images.length;
-for (var p = 0; p < 1; p++) {
-  images[0].parentNode.removeChild(images[0]);
-
+for (var p = 0; p < images.length; p++) {
+  images[p].parentNode.removeChild(images[p]);
 }
-//Displays the image.
-document.body.appendChild(newImage);
+
+
+for (var i = 0; i < 3; i++) {
+  // 0 being the first image and preStuff.length - 1 is  last image.
+  var newImage = getRandomInt(0, preStuff.length - 1);
+  //Displays the image.
+  document.body.appendChild(newImage);
+  console.log('newImage: ', newImage);
+}
+
+
+
 }
