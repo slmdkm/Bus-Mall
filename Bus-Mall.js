9cc3a33ffc70ac11b5c8953c9689d8d84139b936
SAM <samanthamorales8267@gmail.com>
2017-03-04 12:00:10 -0800

    Chart Appended with Data

--- Bus-Mall.js
+++ Bus-Mall.js
@@ -26,6 +26,10 @@
 var allProductsNames = ['bag',  'banana',  'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
 var allProducts = [];
+var clickChart = [];
+var samData = localStorage.getItem('samChart');
+// myChart.push.data.dataset[0].data(this.tally);
+// data.dataset[0].data.
 
 //Now create Constructor Function
 function ProductsConstructor(name, filepath, tally, views) {
@@ -34,9 +38,6 @@ function ProductsConstructor(name, filepath, tally, views) {
   this.tally = 0;//number of clicks
   this.views = 0;//number of times shown
   allProducts.push(this);
-  var myChart = [];
-  // myChart.push.data.dataset[0].data(this.tally);
-  // data.dataset[0].data.
 
 }
 
@@ -120,6 +121,8 @@ var tracker = {
         tracker.tallyVotes(e.target.id);
         tracker.displayImages();
       }
+
+      dataSet1();
     },
 
   //so if elId is a bag it will vote 1, if it's something else it will pass until it finds bag and then tallies it.  It is finding the object so it matches the clicked image....
@@ -139,6 +142,7 @@ var tracker = {
   },
 
   renderResults: function () {
+    console.log('renderResults just got called');
     //createUL
     //creat each of the LI and add content
     //append each li to the ul
@@ -150,13 +154,22 @@ var tracker = {
     for (var i in allProducts) {
       var liEl = document.createElement('li');
 
-      liEl.textContent = allProducts[i].name + ':         ' + allProducts[i].tally + ' votes';
+      liEl.textContent = allProducts[i].name + ':\t\t\t\t' + allProducts[i].tally + ' votes';
       ulEl.appendChild(liEl);
     }
 
     this.showResultsEl.appendChild(ulEl);
+    drawChart();
   },
 };
+
+function dataSet1(){
+  console.log('dataSet1 just got called');
+  for (var i=0; i < allProducts.length; i++){
+    clickChart[i]= allProducts[i].tally;
+  }
+  console.log('clickChart: ', clickChart);
+}
 
 //Event is listening to image and section
 tracker.imageAlbumEl.addEventListener('click', tracker.clickHandler);
@@ -164,79 +177,8 @@ tracker.imageAlbumEl.addEventListener('click', tracker.clickHandler);
 tracker.displayImages();
 
 
-'use strict';
-
-// //1.Charts
-function addDataChart(){
-  // for (var i in allProducts){
-
-  for (var i =0; i < allProducts.length; i++){
-    this.chartLabels[i] = allProducts[i].name;
-    this.chartTally[i] = allProducts[i].tally;
-  }
-}
 
 
-  // 2.var myChart = document.getElementById("myChart");
-  //var ctx = myChart.getContext ('2d');
-
-  var ctx = document.getElementById('myChart').getContext('2d');
-
-  // 3.tracker.addChartData();
-
-  // tracker.addDataChart();
-  //  myChart.data.datasets[0].data.push(this.tally);
-  //  => will get you the data array
-
-  //5.This is an object literal
-  var myChart = new Chart(ctx,{
-
-    //6.type of property, the type of chart is the bar
-    type: 'bar',
-
-    //7.data property,
-    data: {
-
-    //8.productImageNames-this will hold the name of each product image
-      // labels:
-      // tracker.chartLabels,
-
-      labels:['bag',  'banana',  'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'],
-
-
-      //array of 1 object, and more properites, this is a nested layer of objects.
-
-      datasets: [{
-
-        //Data starts with zeros and then increments
-
-        label: '# of Votes',
-        //Holds tally for each product image
-
-        // data: tracker.chartTally,
-        data: [1,2,3,4,5,6,7,8,9],
-
-
-
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1,
-
-      }]
-    },
-
-  });
+//JSON
+// var stringProducts = JSON.stringify(allProducts);
+// localStorage.setItem('products', stringProducts);
--- Charts.js
+++ Charts.js
@@ -1,41 +1,31 @@
-'use strict';
+// 1.Charts
 
-// //1.Charts
-function addDataChart(){
-  // for (var i in allProducts){
+//2.Get the context of the canvas element we want to select
+//var myChart = document.getElementById("myChart");
+//var ctx = myChart.getContext ('2d');
 
-  for (var i =0; i < allProducts.length; i++){
-    this.chartLabels[i] = allProducts[i].name;
-    this.chartTally[i] = allProducts[i].tally;
-  }
-}
+var ctx = document.getElementById('myChart').getContext('2d');
 
+// 3.tracker.addChartData();
 
-  // 2.var myChart = document.getElementById("myChart");
-  //var ctx = myChart.getContext ('2d');
+// tracker.addDataChart();
+//  myChart.data.datasets[0].data.push(this.tally);
+//  => will get you the data array
+//5.This is an object literal
 
-  var ctx = document.getElementById('myChart').getContext('2d');
-
-  // 3.tracker.addChartData();
-
-  // tracker.addDataChart();
-  //  myChart.data.datasets[0].data.push(this.tally);
-  //  => will get you the data array
-
-  //5.This is an object literal
-  var myChart = new Chart(ctx,{
+function drawChart() {
+  console.log('drawChart just got called');
+  var myChart = new Chart(ctx, {
 
     //6.type of property, the type of chart is the bar
     type: 'bar',
-
     //7.data property,
     data: {
-
-    //8.productImageNames-this will hold the name of each product image
+      //8.productImageNames-this will   hold the name of each product image
       // labels:
       // tracker.chartLabels,
 
-      labels:['bag',  'banana',  'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'],
+    labels:['bag',  'banana',  'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'],
 
 
       //array of 1 object, and more properites, this is a nested layer of objects.
@@ -44,12 +34,12 @@ function addDataChart(){
 
         //Data starts with zeros and then increments
 
-        label: '# of Votes',
+        label: 'Product Results!',
         //Holds tally for each product image
 
-        // data: tracker.chartTally,
-        data: [1,2,3,4,5,6,7,8,9],
-
+        // data: tracker.tallyVotes,
+        data: clickChart,
+        // data: displayedChart,
 
 
         backgroundColor: [
@@ -58,7 +48,21 @@ function addDataChart(){
           'rgba(255, 206, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
           'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)'
         ],
         borderColor: [
           'rgba(255,99,132,1)',
@@ -66,7 +70,21 @@ function addDataChart(){
           'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
           'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          'rgba(255, 159, 64, 1)',
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)'
         ],
         borderWidth: 1,
 
@@ -74,3 +92,4 @@ function addDataChart(){
     },
 
   });
+}
